da331d32512b704bfbcc553851ddd6769c3c744f
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-29 16:38:31 +0200

    pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import Movies from "./components/Movies/Movies";
 import TvSeries from "./components/TvSeries/TvSeries";
@@ -9,11 +10,8 @@ import Nothing from "./components/Nothing/Nothing";
 import Intro from "./components/Intro/Intro";
 import ContentLoader from "./components/ContentLoader/ContentLoader";
 import PageLoader from "./components/PageLoader/PageLoader";
+import CustomPages from "./components/CustomPages/CustomPages";
 //import Search from "./components/Search/Search";
-
-const MOVIE_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1`;
-
-const TVSERIES_API = `https://api.themoviedb.org/3/tv/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1`;
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -21,12 +19,15 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(true);
   const [loader, setLoader] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pageNum, setPageNum] = useState();
 
   const getMovie = (API) => {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
+        setPageNum(data?.total_pages);
         setLoading(false);
       });
   };
@@ -36,17 +37,22 @@ export default function App() {
       .then((res) => res.json())
       .then((data) => {
         setSeries(data.results);
+        setPageNum(data?.total_pages);
         setLoading(false);
       });
   };
 
   useEffect(() => {
-    getMovie(MOVIE_API);
-  }, []);
+    getMovie(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=${page}`
+    );
+  }, [page]);
 
   useEffect(() => {
-    getSeries(TVSERIES_API);
-  }, []);
+    getSeries(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=${page}`
+    );
+  }, [page]);
 
   const movieChangeHandler = (e) => {
     e.preventDefault();
@@ -54,14 +60,16 @@ export default function App() {
     var length = e.target.value.length;
     if (length >= 3) {
       fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+        `https://api.themoviedb.org/3/search/movie?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1&include_adult=false&query=${e.target.value}&page=${page}`
       )
         .then((res) => res.json())
         .then((data) => {
           setMovies(data.results);
         });
     } else if (length === 0) {
-      getMovie(MOVIE_API);
+      getMovie(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=${page}`
+      );
     }
   };
 
@@ -71,14 +79,16 @@ export default function App() {
     var length = e.target.value.length;
     if (length >= 3) {
       fetch(
-        `https://api.themoviedb.org/3/search/tv?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+        `https://api.themoviedb.org/3/search/tv?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=1&include_adult=false&query=${e.target.value}&page=${page}`
       )
         .then((res) => res.json())
         .then((data) => {
           setSeries(data.results);
         });
     } else if (length === 0) {
-      getSeries(TVSERIES_API);
+      getSeries(
+        `https://api.themoviedb.org/3/tv/top_rated?api_key=8c2bc4e84cc4e0bebe9e71ffd52ae730&language=en-US&page=${page}`
+      );
     }
   };
 
@@ -130,6 +140,7 @@ export default function App() {
                       ) : (
                         <Nothing />
                       )}
+                      <CustomPages setPage={setPage} pageNum={pageNum} />
                     </div>
                   )}
                 </>
@@ -166,6 +177,7 @@ export default function App() {
                       ) : (
                         <Nothing />
                       )}
+                      <CustomPages setPage={setPage} pageNum={pageNum} />
                     </div>
                   )}
                 </>
---
+++ src/components/CustomPages/CustomPages.js
@@ -0,0 +1,24 @@
+import { Pagination } from "@mui/material";
+import React from "react";
+
+export default function CustomPages({ setPage, pageNum }) {
+  const changeHandler = (page) => {
+    setPage(page);
+    //window.scroll(0, document.getElementById("#search"));
+    window.scroll(0, 930);
+  };
+
+  return (
+    <div className="page-cont">
+      <Pagination
+        onChange={(e) => changeHandler(e.target.textContent)}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+        variant="outlined"
+        count={pageNum}
+      />
+    </div>
+  );
+}
